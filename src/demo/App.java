84ef621192c7dd8e9c447d6763a3fe5d873d07a5
Sailesh-Patel1 <Sailesh_patel_bsc@hotmail.co.uk>
2024-01-12 11:27:35 +0000

    Initial commit to github

---
+++ src/demo/App.java
@@ -0,0 +1,31 @@
+package demo;
+
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+	public static void main(String[] args) {
+//		type name = value
+		byte byt = 25;
+		short shrt = 255;
+		int i = 2_100_000_000;
+//		without the L java thinks it's an int
+		long l = 99999999999999L;
+//      need to add an f to the end of the float value, or java thinks its a double 
+		float f = 44.94f;
+		double d = 44.94;
+		
+		boolean b = true;
+		
+//		need to use ' for a single character 
+		char c = 'j'; 
+//		need to use " for strings which are sequence of characters 
+		String s = "";
+		
+		
+		
+	}
+	
+}
